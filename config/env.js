e567a61afebf83f95d686ef3db68655eaaf43ccc
Mayank Panchal <mayank@screetract.com>
2025-02-11 11:24:27 +0530

    urls update

--- config/env.js
+++ config/env.js
@@ -1,9 +1,19 @@
-import { config } from "dotenv";
+// import { config } from "dotenv";
 
-config({
-    path: `.env.${
-        process.env.NODE_ENV || "development"
-    }.local`,
-});
+// config({
+//     path: `.env.${
+//         process.env.NODE_ENV || "development"
+//     }.local`,
+// });
 
-export const { PORT, NODE_ENV, DB_URI } = process.env;
+// # Port
+export const PORT = 5000;
+
+// # ENVIRONMENT
+export const NODE_ENV = development;
+
+// # Database
+export const DB_URI =
+    "mongodb+srv://mayankpanchal1801:mypass@cluster0.f2n2b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// export const { PORT, NODE_ENV, DB_URI } = process.env;
